b822c050b269778f34897ce0567cfff910b2d09e
Reza Ageng <rezaageng5@gmail.com>
2022-04-04 13:54:16 +0700

    add orbit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,16 @@ import { Canvas } from '@react-three/fiber';
 import type { NextPage } from 'next';
 import { AxesHelper } from 'three';
 import Ayaka from './components/Ayaka';
+
 import Layout from './components/Layout';
 
 const Home: NextPage = () => (
   <Layout>
-    <Canvas>
+    <Canvas orthographic={true} camera={{ zoom: 200, position: [0, 0, 100] }}>
       <ambientLight intensity={0.5} />
-      <OrbitControls />
-      <directionalLight position={[10, 10, 10]} />
-      <primitive object={new AxesHelper(10)} />
-      <Ayaka position={[0, -1.5, 0]} />
+      <OrbitControls maxZoom={1000} minZoom={100} />
+      <directionalLight position={[10, 10, 10]} intensity={1} />
+      <Ayaka position={[0, -1.5, 0]} rotation={[0, 0, 0]} />
     </Canvas>
   </Layout>
 );
